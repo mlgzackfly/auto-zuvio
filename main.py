28729b63265c070c926adf319ff68ec7241dce82
mlgzackfly <mlgzackfly@gmail.com>
2023-03-09 16:00:04 +0800

    docs: cancel show rollcall_id

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def courses(user_id, accessToken):
                 if "Zuvio" not in course_data['teacher_name']: # 避免 Zuvio 官方活動之類的課程
                     rollcall_id = check(course_data['course_id'])
                     if rollcall_id != "":  # rollcall_id 不為空的話代表可以簽到
-                        print(f"rollcall_id = {rollcall_id}")
+                        # print(f"rollcall_id = {rollcall_id}")
                         print(" 開放簽到！")
                         print(course_data['course_name'] + checkIn(user_id, accessToken, rollcall_id))
                 else:
